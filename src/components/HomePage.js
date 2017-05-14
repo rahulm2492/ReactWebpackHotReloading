8ef078a1ce87694ea7cd610f3085e87f097e8c0a
Rahul Maheshwari <rahulm@qburst.com>
2017-05-14 17:30:31 +0530

    asdsadsfd

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => (
   <div className="jumbotron center">
-    <h1 className="lead">Welcome to Media Library built with React, Redux, and Redux-saga </h1>
+    <h1 className="lead">RADHA PACKERS</h1>
     <div>
       <Link to="/library">
         <button className="btn btn-lg btn-primary"> Visit Library</button>
