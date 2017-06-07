127c930af86bdba8f28b3bfc554de43a5fe6b832
Rahul Maheshwari <rahulm@qburst.com>
2017-06-08 00:29:32 +0530

    master

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static(path.resolve(__dirname,  'build')));
-
-app.listen(3000,function(){
-console.log("Express Started on Port 3000");
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host,function(){
+console.log('Listening on port %d', server_port);
 });
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
